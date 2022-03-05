7d87b908b204a0198c0eb306b9d2d76f30ce3421
Jai Bellare <jaibellare@gmail.com>
2022-03-05 12:27:07 +0530

    add Catch2 install message

--- cmake/setup_catch2.cmake
+++ cmake/setup_catch2.cmake
@@ -7,11 +7,14 @@
 
 function(setup_catch2)
     include(FetchContent)
+
     FetchContent_Declare(
         Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG f4af9f69265d009a457aa99d1075cfba78652a66 # v3.0.0-preview4
     )
+
+    message(STATUS "Installed Catch2")
 
     FetchContent_MakeAvailable(Catch2)
 endfunction(setup_catch2)
