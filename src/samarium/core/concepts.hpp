5ddee023e5c26c66f7f0a741d31448d83325b297
Jai Bellare <jaibellare@gmail.com>
2022-03-07 16:21:23 +0530

    remove unecessary container concept

--- src/samarium/core/concepts.hpp
+++ src/samarium/core/concepts.hpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2022 Jai Bellare
  * Project homepage: https://github.com/strangeQuark1041/samarium
@@ -41,53 +41,4 @@ concept Arithmetic = requires(T a, T b)
 
 template <typename T, typename... U>
 concept AnyOf = (std::same_as<T, U> || ...);
-
-template <class ContainerType>
-concept Container = requires(ContainerType a, const ContainerType b)
-{
-    requires std::regular<ContainerType>;
-    requires std::swappable<ContainerType>;
-    requires std::destructible<typename ContainerType::value_type>;
-    requires std::same_as<typename ContainerType::reference,
-                          typename ContainerType::value_type&>;
-    requires std::same_as<typename ContainerType::const_reference,
-                          const typename ContainerType::value_type&>;
-    requires std::forward_iterator<typename ContainerType::iterator>;
-    requires std::forward_iterator<typename ContainerType::const_iterator>;
-    requires std::signed_integral<typename ContainerType::difference_type>;
-    requires std::same_as<typename ContainerType::difference_type,
-                          typename std::iterator_traits<
-                              typename ContainerType::iterator>::difference_type>;
-    requires std::same_as<
-        typename ContainerType::difference_type,
-        typename std::iterator_traits<
-            typename ContainerType::const_iterator>::difference_type>;
-    {
-        a.begin()
-        } -> std::same_as<typename ContainerType::iterator>;
-    {
-        a.end()
-        } -> std::same_as<typename ContainerType::iterator>;
-    {
-        b.begin()
-        } -> std::same_as<typename ContainerType::const_iterator>;
-    {
-        b.end()
-        } -> std::same_as<typename ContainerType::const_iterator>;
-    {
-        a.cbegin()
-        } -> std::same_as<typename ContainerType::const_iterator>;
-    {
-        a.cend()
-        } -> std::same_as<typename ContainerType::const_iterator>;
-    {
-        a.size()
-        } -> std::same_as<typename ContainerType::size_type>;
-    {
-        a.max_size()
-        } -> std::same_as<typename ContainerType::size_type>;
-    {
-        a.empty()
-        } -> std::same_as<bool>;
-};
 } // namespace sm::concepts
