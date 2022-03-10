bef611fb5f7d84d94ca2b4becd1871a4844c7fa2
Jai Bellare <jaibellare@gmail.com>
2022-03-10 09:27:02 +0530

    simplify ccache usage

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -3,7 +3,7 @@
 # See <https://opensource.org/licenses/MIT/> or LICENSE.md
 # Project homepage: <https://github.com/strangeQuark1041/samarium>
 
-# from https://crascit.com/2016/04/09/using-ccache-with-cmake/#h-improved-functionality-from-cmake-3-4
+# https://www.reddit.com/r/cpp/comments/t2yoay/comment/hyrgm63/
 
 cmake_minimum_required(VERSION 3.16)
 
@@ -11,27 +11,7 @@ function(use_ccache)
     find_program(CCACHE_PROGRAM ccache)
     if(CCACHE_PROGRAM)
         message(STATUS "ccache found: ${CCACHE_PROGRAM}")
-        # Set up wrapper scripts
-        set(C_LAUNCHER   "${CCACHE_PROGRAM}")
-        set(CXX_LAUNCHER "${CCACHE_PROGRAM}")
-        configure_file(./cmake/launch-c.in   launch-c)
-        configure_file(./cmake/launch-cxx.in launch-cxx)
-        execute_process(COMMAND chmod a+rx
-                        "${CMAKE_BINARY_DIR}/launch-c"
-                        "${CMAKE_BINARY_DIR}/launch-cxx"
-        )
-
-        if(CMAKE_GENERATOR STREQUAL "Xcode")
-            # Set Xcode project attributes to route compilation and linking
-            # through our scripts
-            set(CMAKE_XCODE_ATTRIBUTE_CC         "${CMAKE_BINARY_DIR}/launch-c")
-            set(CMAKE_XCODE_ATTRIBUTE_CXX        "${CMAKE_BINARY_DIR}/launch-cxx")
-            set(CMAKE_XCODE_ATTRIBUTE_LD         "${CMAKE_BINARY_DIR}/launch-c")
-            set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${CMAKE_BINARY_DIR}/launch-cxx")
-        else()
-            # Support Unix Makefiles and Ninja
-            set(CMAKE_C_COMPILER_LAUNCHER   "${CMAKE_BINARY_DIR}/launch-c")
-            set(CMAKE_CXX_COMPILER_LAUNCHER "${CMAKE_BINARY_DIR}/launch-cxx")
-        endif()
+        set(CMAKE_C_COMPILER_LAUNCHER "ccache")
+        set(CMAKE_CXX_COMPILER_LAUNCHER "ccache")
     endif()
 endfunction()
