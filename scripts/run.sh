5a96bbf7b4b85ae3d19a6f345d32c41e4942d660
Jai Bellare <jaibellare@gmail.com>
2022-03-24 23:01:24 +0530

    don't build extra tests when not needed

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,6 @@ find ./build -name "*.gcda" -type f -delete &> /dev/null
 find ./build -name "*.gcno" -type f -delete &> /dev/null
 rm -f ./build/test/samarium_tests
 
+echo "Compiling..."
 ./scripts/build.sh
 ~/bin/tryrun ./build/test/samarium_tests
