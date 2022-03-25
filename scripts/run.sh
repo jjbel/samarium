f9ed2ff6d67d70316c39e509f5747a94580484e9
Jai Bellare <jaibellare@gmail.com>
2022-03-25 17:04:44 +0530

    scripts/run: run cmake configure in clean directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "./build" ]; then
+    cmake --preset=home &> /dev/null
+fi
+
 find ./build -name "*.gcda" -type f -delete &> /dev/null
 find ./build -name "*.gcno" -type f -delete &> /dev/null
 rm -f ./build/test/samarium_tests
