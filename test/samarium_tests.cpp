f247cba632e4091fbecdcd372fafd9fe89dd68a5
Jai Bellare <jaibellare@gmail.com>
2022-03-25 17:06:58 +0530

    changes

--- test/samarium_tests.cpp
+++ test/samarium_tests.cpp
@@ -5,15 +5,15 @@
  * Project homepage: https://github.com/strangeQuark1041/samarium
  */
 
-#include "../src/samarium/graphics/colors.hpp"
-#include "../src/samarium/graphics/gradients.hpp"
-#include "../src/samarium/gui/Window.hpp"
-#include "../src/samarium/gui/sfml.hpp"
-#include "../src/samarium/math/Dual.hpp"
-#include "../src/samarium/physics/collision.hpp"
-#include "../src/samarium/physics/fluid/Fluid.hpp"
-#include "../src/samarium/util/file.hpp"
-#include "../src/samarium/util/format.hpp"
+#include "samarium/graphics/colors.hpp"
+#include "samarium/graphics/gradients.hpp"
+#include "samarium/samarium.hpp"
+
+auto tmp(sm::util::Stopwatch& watch)
+{
+    fmt::print(stderr, "{:4.2f}\n", 1.0 / watch.time().count());
+    watch.reset();
+}
 
 void App();
 
@@ -21,27 +21,57 @@ int main() { App(); }
 
 void App()
 {
-    using namespace sm;
 
-    const auto dims = Dimensions{512, 512};
-    const auto bbox = BoundingBox<i64>{{}, dims.as<i64>()};
-    auto fluid      = Fluid{};
-
-    auto window = Window{{dims}};
-
-    size_t frame{};
-    while (window.is_open() && ++frame <= 200)
+    const auto tmp = [](sm::util::Stopwatch& w)
     {
-        fmt::print(stderr, "\n{}: ", window.frame_counter);
-        window.get_input();
+        const auto r = 1.0 / w.time().count();
+        fmt::print(stderr, "{:4.2f}\n",
+                   r); // print to stderr for no line buffering
+        w.reset();
+        return r;
+    };
 
-        if (const auto pos = window.mouse.pos->as<i64>(); window.mouse.left && bbox.contains(pos))
-        {
-            print("Click!", pos);
-            // image[pos.as<u64>()] = colors::white;
-        }
+    using namespace sm::literals;
+    auto rn = sm::Renderer{sm::Image{sm::dims720}};
 
-        // window.draw(fluid.to_image());
-        window.display();
-    }
+    const auto gravity = -100.0_y;
+
+    const sm::Vector2 anchor   = 30.0_y;
+    const auto rest_length     = 14.0;
+    const auto spring_constant = 100.0;
+
+    auto p1 =
+        sm::Particle{.pos = {}, .vel = {50, 0}, .radius = 3, .mass = 40, .color = sm::colors::red};
+    auto p2 = p1;
+
+    const auto l = sm::LineSegment{{-30, -30}, {30, -9}};
+
+    const auto dims         = rn.image.dims.as<double>();
+    const auto viewport_box = rn.viewport_box();
+    sm::util::Stopwatch watch{};
+
+    const auto run_every_frame = [&]
+    {
+        rn.fill(sm::Color{16, 18, 20});
+        p1.apply_force(p1.mass * gravity);
+        const auto spring = p1.pos - anchor;
+        const auto force  = spring.with_length(spring_constant * (rest_length - spring.length()));
+        p1.apply_force(force);
+        p1.update();
+        auto dual = sm::Dual{p1, p2};
+        sm::phys::collide(dual, l);
+        for (auto&& i : viewport_box) sm::phys::collide(dual, i);
+        std::tie(p1, p2) = std::tuple{dual.now, dual.prev};
+
+        rn.draw_line_segment(l, sm::gradients::blue_green, 0.4);
+        rn.draw_line_segment(sm::LineSegment{anchor, p1.pos}, "#c471ed"_c, .6);
+        rn.draw(p1);
+        p2 = p1;
+        rn.render();
+        tmp(watch);
+    };
+
+    run_every_frame();
+    auto window = sm::Window{{rn.image.dims, "Collision", 60}};
+    window.run(rn, run_every_frame);
 }
