cd3716f537bdf196eb3e9a934c2124b5679c6ae4
Jai Bellare <jaibellare@gmail.com>
2022-04-01 17:27:54 +0530

    rework random implementation

--- src/samarium/util/random.hpp
+++ src/samarium/util/random.hpp
@@ -7,49 +7,80 @@
 
 #pragma once
 
+#include <algorithm>
 #include <array>
-#include <random>
 
+#include "../math/BoundingBox.hpp"
 #include "../math/interp.hpp"
 
 namespace sm::random
 {
-static auto cache_length = size_t{1000};
-
-static auto current = size_t{0};
-
-namespace detail
+class LinearCongruentialGenerator
 {
-static auto get_random(size_t size)
-{
-    // std::random_device rand_dev;
-    std::vector<f64> values(size);
+  public:
+    constexpr LinearCongruentialGenerator(u16 new_seed = 0) noexcept : seed{new_seed} {}
 
-    std::mt19937 generator(247);
-    std::uniform_real_distribution<f64> distribution(0., 1.);
-    std::generate(values.begin(), values.end(), [&] { return distribution(generator); });
-    return values;
+[[nodiscard]] constexpr auto next() noexcept
+{
+    seed = seed * a + c;
+    return (seed >> 16) & 0x7fff;
 }
-} // namespace detail
 
-static std::vector<f64> cache = detail::get_random(4000u);
+u32 seed;
 
-auto fill_cache(size_t size) { cache = detail::get_random(size); }
+private:
+const u16 a = u16(214013);
+const u16 c = u16(2531011);
+};
 
-auto random() { return cache[current++ % cache_length]; }
+template <size_t size> class Cache
+{
+    std::array<f64, size> cache;
+    u64 current_index{};
 
-template <typename T> [[nodiscard]] auto rand_range(Extents<T> range_)
+  public:
+    constexpr Cache() noexcept
+    {
+        auto rng = sm::random::LinearCongruentialGenerator{};
+        std::generate(cache.begin(), cache.end(),
+                      [&rng] { return static_cast<f64>(rng.next()) / 32768.0; });
+    }
+
+    [[nodiscard]] constexpr auto operator[](u32 index) noexcept { return cache[index]; }
+
+    [[nodiscard]] constexpr auto next() noexcept
+    {
+        current_index++;
+        return cache[current_index];
+    }
+
+    constexpr auto reseed(u32 new_seed) noexcept
+    {
+        auto rng = sm::random::LinearCongruentialGenerator{new_seed};
+        std::generate(cache.begin(), cache.end(),
+                      [&rng] { return static_cast<f64>(rng.next()) / 32768.0; });
+
+        current_index = 0UL;
+    }
+};
+
+static constinit auto cache = Cache<1024UL>{};
+
+constexpr auto random() { return cache.next(); }
+
+template <typename T> [[nodiscard]] constexpr auto rand_range(Extents<T> range_) noexcept
 {
     return static_cast<T>(range_.lerp(random()));
 }
 
-[[nodiscard]] auto rand_vector(BoundingBox<f64> bounding_box)
+[[nodiscard]] constexpr auto rand_vector(BoundingBox<f64> bounding_box) noexcept
 {
     return Vector2{rand_range<f64>({bounding_box.min.x, bounding_box.max.x}),
                    rand_range<f64>({bounding_box.min.y, bounding_box.max.y})};
 }
 
-[[nodiscard]] auto rand_vector(Extents<f64> radius_range, Extents<f64> angle_range)
+[[nodiscard]] constexpr auto rand_vector(Extents<f64> radius_range,
+                                         Extents<f64> angle_range) noexcept
 {
     return Vector2::from_polar({.length = rand_range<f64>({radius_range.min, radius_range.max}),
                                 .angle  = rand_range<f64>({angle_range.min, angle_range.max})});
