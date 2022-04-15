bfc2dc20fb623de90501363c2a7136931644ddc0
Jai Bellare <jaibellare@gmail.com>
2022-04-15 19:41:19 +0530

    add additional SFML compatibility

--- src/samarium/gui/sfml.hpp
+++ src/samarium/gui/sfml.hpp
@@ -8,12 +8,26 @@
 #pragma once
 
 #include "SFML/Graphics.hpp"
+#include "SFML/Graphics/Color.hpp"
 #include "SFML/Window.hpp"
+
+#include "../graphics/Color.hpp"
+#include "../math/Vector2.hpp"
+#include <SFML/System/Vector2.hpp>
 
 namespace sm
 {
 template <typename T> [[nodiscard]] constexpr Vector2_t<T> sfml(sf::Vector2<T> vec) noexcept
 {
     return {vec.x, vec.y};
+}
+template <typename T> [[nodiscard]] constexpr sf::Vector2<T> sfml(Vector2_t<T> vec) noexcept
+{
+    return {vec.x, vec.y};
+}
+
+[[nodiscard]] inline auto sfml(Color color)
+{
+    return sf::Color{color.r, color.g, color.b, color.a};
 }
 } // namespace sm
