b9e31415ccc50b53fd8f0291f14e6ca76103e226
Jai Bellare <jaibellare@gmail.com>
2022-04-21 18:51:53 +0530

    changes

--- test/test.cpp
+++ test/test.cpp
@@ -1,1 +1,13 @@
-int main() {}
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Jai Bellare
+ * See <https://opensource.org/licenses/MIT/> or LICENSE.md
+ * Project homepage: https://github.com/strangeQuark1041/samarium
+ */
+
+#include "samarium/samarium.hpp"
+#include "samarium/util/random.hpp"
+
+using namespace sm;
+
+int main() {for(auto i : range(10)) print(random::choice({1, -2, -4, 5})); }
