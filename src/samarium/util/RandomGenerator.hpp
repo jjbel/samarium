5038aedeef5ea1973c8d76bac50eea80bdb7e399
Jai Bellare <jaibellare@gmail.com>
2022-04-27 11:02:42 +0530

    fix namespacing

--- src/samarium/util/RandomGenerator.hpp
+++ src/samarium/util/RandomGenerator.hpp
@@ -67,8 +67,8 @@ struct RandomGenerator
                                            Vector2 sample_region_size,
                                            u64 sample_count = 30UL) -> std::vector<Vector2>;
 };
-} // namespace sm
 
 using Image       = Grid<Color>;
 using ScalarField = Grid<f64>;
 using VectorField = Grid<Vector2>;
+} // namespace sm
