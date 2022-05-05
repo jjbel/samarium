713768b5d896250d4977a64210b7566ed959213c
Jai Bellare <jaibellare@gmail.com>
2022-05-05 12:57:49 +0530

    math.hpp: add docs

--- src/samarium/math/math.hpp
+++ src/samarium/math/math.hpp
@@ -16,11 +16,29 @@ namespace sm::math
 {
 constexpr inline auto epsilon = 1.e-4;
 
-template <concepts::FloatingPoint T> [[nodiscard]] constexpr auto almost_equal(T a, T b) noexcept
+/**
+ * @brief               Check if 2 floating point values are equal
+ *
+ * @tparam T            Floating point type
+ * @param  a            First value
+ * @param  b            Second value
+ * @param  threshold    threshold within which to compare a and b, by default sm::math::epsilon
+ * @return true         if a and b are within threshold else false
+ */
+template <concepts::FloatingPoint T>
+[[nodiscard]] constexpr auto almost_equal(T a, T b, f64 threshold = epsilon) noexcept
 {
-    return std::abs(a - b) <= epsilon;
+    return std::abs(a - b) <= threshold;
 }
 
+/**
+ * @brief               Minimum of 2 values
+ *
+ * @tparam T
+ * @param  value0       First value
+ * @param  value1       Second value
+ * @return T
+ */
 template <typename T> [[nodiscard]] constexpr auto min(T value0, T value1) noexcept
 {
     if (value0 < value1) { return value0; }
@@ -30,6 +48,14 @@ template <typename T> [[nodiscard]] constexpr auto min(T value0, T value1) noexc
     }
 }
 
+/**
+ * @brief               Maximum of 2 values
+ *
+ * @tparam T
+ * @param  value0       First value
+ * @param  value1       Second value
+ * @return T
+ */
 template <typename T> [[nodiscard]] constexpr auto max(T value0, T value1) noexcept
 {
     if (value0 > value1) { return value0; }
@@ -39,6 +65,13 @@ template <typename T> [[nodiscard]] constexpr auto max(T value0, T value1) noexc
     }
 }
 
+/**
+ * @brief               Raise a number to a fixed power
+ *
+ * @tparam n            Exponent
+ * @param  x            Base
+ * @return auto
+ */
 template <u32 n> [[nodiscard]] constexpr auto power(auto x) noexcept
 {
     if constexpr (n == 0) { return 1; }
@@ -46,44 +79,91 @@ template <u32 n> [[nodiscard]] constexpr auto power(auto x) noexcept
     return x * power<n - 1>(x);
 }
 
+/**
+ * @brief               Convert from radians to degrees
+ *
+ * @param  angle        Angle in radians
+ * @return auto
+ */
 [[nodiscard]] constexpr auto to_degrees(concepts::FloatingPoint auto angle) noexcept
 {
     return angle * 180.0 / std::numbers::pi;
 }
 
+/**
+ * @brief               Convert from degrees to radians
+ *
+ * @param  angle        Angle in degrees
+ * @return auto
+ */
 [[nodiscard]] constexpr auto to_radians(concepts::FloatingPoint auto angle) noexcept
 {
     return angle / 180.0 * std::numbers::pi;
 }
 
+/**
+ * @brief               Absolute value of a value
+ *
+ * @tparam T            A numeric type
+ * @param  x            Value
+ * @return T
+ */
 template <concepts::Number T> [[nodiscard]] constexpr auto abs(T x) noexcept
 {
     return x >= T{} ? x : -x;
 }
 
+/**
+ * @brief               Sign of an unsigned type
+ *
+ * @tparam T            Unsigned Type
+ * @param  x            Value
+ * @return 1            if x is greater than 0 else 0
+ */
 template <typename T>
 [[nodiscard]] constexpr auto sign(T x, std::false_type /* is_signed */) noexcept -> i32
 {
     return T(0) < x;
 }
 
+/**
+ * @brief               Sign of a signed type
+ *
+ * @tparam T            Signed type
+ * @param x             Value
+ * @return              -1 if x < 0, 1 if x > 0, 0 is x == 0
+ */
 template <typename T>
 [[nodiscard]] constexpr auto sign(T x, std::true_type /* is_signed */) noexcept -> i32
 {
     return (T(0) < x) - (x < T(0));
 }
 
+/**
+ * @brief               Sign of a value
+ *
+ * @tparam T            Numeric type
+ * @param x             Value
+ * @return              -1 if x < 0, 1 if x > 0, 0 is x == 0
+ */
 template <typename T> [[nodiscard]] constexpr auto sign(T x) noexcept -> i32
 {
     return sign(x, std::is_signed<T>{});
 }
 
+/**
+ * @brief               Modulus of 2 floating-point values
+ *
+ * @tparam T
+ * @param  x            First value
+ * @param  y            Second value
+ * @return T
+ */
 template <concepts::FloatingPoint T> constexpr auto mod(T x, T y) noexcept
 {
     return x - trunc(x / y) * y;
 }
 // https://stackoverflow.com/a/67098028
-
 } // namespace sm::math
 
 namespace sm::literals
