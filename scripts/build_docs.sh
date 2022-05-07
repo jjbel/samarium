4ad290e5d9379b26e3ba2496e0cd07b8411d9f51
Jai Bellare <jaibellare@gmail.com>
2022-05-07 10:49:40 +0530

    edit docs script

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -4,11 +4,13 @@ set -e
 
 git checkout gh-pages
 
-rm ./README.md
+rm --force ./README.md
 
 cmake --preset=default
 cmake --build build --target docs
 
 cp -R ./build/docs/* .
 
+git add .
 git status
+git commit -m "build docs"
