2e714e04342befe706f859cf369771320b2e0b9a
Jai Bellare <jaibellare@gmail.com>
2022-06-14 12:41:48 +0530

    changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -11,25 +11,7 @@
 using namespace sm;
 using namespace sm::literals;
 
-struct particle_t
-{
-    Vector2 pos{};
-    Vector2 vel;
-    f64 speed;
-    Vector2 old_pos{pos};
-};
-
-struct Fluid
-{
-
-};
-
-
 int main()
 {
-    auto app = App{{.dims = {1000, 1000}}};
-    // auto fluid = Fluid{};
-
-    const auto draw = [&] { app.fill("#06060c"_c); };
-    app.run(draw);
+    
 }
