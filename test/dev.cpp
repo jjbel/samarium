0bd01cb0301b37eedba78f2affcdb427d470b4fb
Jai Bellare <jaibellare@gmail.com>
2022-07-01 19:05:17 +0530

    dev changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -9,49 +9,78 @@
 #include "samarium/samarium.hpp"
 
 #include "range/v3/view/enumerate.hpp"
-
+#include "range/v3/view/iota.hpp"
+#include "range/v3/view/transform.hpp"
+#include "range/v3/view/zip.hpp"
 using namespace sm;
 using namespace sm::literals;
 
 int main()
 {
-    auto app         = App{{.dims{1280, 720}}};
-    const auto count = 200;
-    const auto size  = Vector2{70.0, 30.0};
-
-    auto plot = std::vector<Vector2>(count);
-
+    auto app   = App{{.dims{1800, 900}}};
     auto watch = Stopwatch{};
 
-    auto rand = RandomGenerator{};
-    // auto noise = util::noise{};
+    const auto scale     = .4;
+    const auto cell_size = 1.0;
+    const auto dims      = Dimensions::combine(50);
+
+    auto ps     = ParticleSystem{1000};
+    auto forces = VectorField{dims};
+    auto rand   = RandomGenerator{};
+
+    for (auto& particle : ps)
+    {
+        particle.pos = rand.vector({.min = Vector2{0.0, 0.0}, .max = dims.as<f64>() * cell_size});
+        particle.radius = 0.18;
+    }
+
+    for (auto [pos, force] : forces.enumerate_2d())
+    {
+        const auto noisy_angle =
+            noise::perlin_2d(pos.as<f64>() +
+                                 Vector2::combine(10) * static_cast<f64>(app.frame_counter),
+                             {.scale = scale}) *
+            math::two_pi;
+        force = Vector2::from_polar({.length = 1.0, .angle = noisy_angle});
+    }
+
+    const auto update = [&](f64 dt)
+    {
+        for (auto [i, particle] : ranges::views::enumerate(ps))
+        {
+            const auto pos   = (particle.pos / cell_size).as<u64>();
+            const auto force = forces.at_or(pos, Vector2{0.0, 0.0});
+            // particle.vel *= 0.9;
+
+            particle.apply_force(force);
+        }
+
+        ps.update(dt);
+        ps.for_each(
+            [=](Particle& particle)
+            {
+                particle.pos.x = math::wrap_max(particle.pos.x, dims.x * cell_size);
+                particle.pos.y = math::wrap_max(particle.pos.y, dims.y * cell_size);
+                particle.vel.clamp_length({0.0, 3.0});
+            });
+    };
 
     const auto draw = [&]
     {
-        if (app.mouse.left) { app.transform.pos += app.mouse.current_pos - app.mouse.old_pos; }
+        // app.fill("#161619"_c.with_multiplied_alpha(0.01));
+        app.draw(Circle{.radius = 240}, {.fill_color = "#000000"_c.with_multiplied_alpha(0.004)});
 
-        const auto scale = 1.0 + 0.1 * app.mouse.scroll_amount;
-        app.transform.scale *= Vector2::combine(scale);
-        const auto mouse_pos = app.mouse.current_pos;
-        app.transform.pos    = mouse_pos + scale * (app.transform.pos - mouse_pos);
+        for (const auto& particle : ps)
+        {
+            const auto color = "#ff1745"_c.with_multiplied_alpha(0.05);
 
-        app.fill("#16161c"_c);
-        // app.draw_polyline(plot, "#4542ff"_c, 0.1);
-        app.draw_world_space(
-            [&](Vector2 pos)
-            {
-                auto noise =
-                    noise::perlin_2d(pos.as<f64>(),
-                                     {.scale = 1.0, .detail = 8, .seed = 1, .roughness = 0.6}) *
-                    255.0;
-                return Color::from_grayscale(static_cast<u8>(noise));
-            });
+            const auto pos =
+                app.transform.apply_inverse(particle.pos * app.dims().as<f64>() / dims.as<f64>());
+            app.draw(Circle{pos, particle.radius}, {.fill_color = color});
+        }
 
-        print(watch.seconds() * 1000.0);
-        // print(std::round(1.0 / watch.seconds()));
-        watch.reset();
+        // print(std::round(watch.current_fps()));
     };
-    app.run(draw);
-    // for (auto i : range(100, 200)) { print(static_cast<f64>(ValueNoise_2D(0, i, 7, 0.5, 0) *
-    // 255)); }
+
+    app.run(update, draw);
 }
