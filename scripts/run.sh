ba1cf3bdd0af94345c6200367a75c79626dbe42b
Jai Bellare <jaibellare@gmail.com>
2022-07-07 20:41:29 +0530

    scripts/run.sh: extract executable to variable

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,14 @@ if [ ! -d "./build" ]; then
     cmake --preset=dev &> /dev/null
 fi
 
+# PROGRAM=./build/test/samarium_tests
+PROGRAM=./build/benchmarks/samarium_benchmarks
+
 find ./build -name "*.gcda" -type f -delete &> /dev/null
 find ./build -name "*.gcno" -type f -delete &> /dev/null
-rm -f ./build/test/samarium_tests
+rm -f ${PROGRAM}
 
 echo "Compiling..."
 ./scripts/build.sh
 echo "Done"
-~/bin/tryrun ./build/test/samarium_tests
+~/bin/tryrun ${PROGRAM}
