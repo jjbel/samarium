776b1ad648a33d5f07f4e25a1530deedb5dc9562
Jai Bellare <jaibellare@gmail.com>
2022-07-29 16:49:10 +0530

    dev changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@
 mold -run cmake --build --preset=default &> ./build/output
 # sed -i 's#home##g' ./build/output
 # sed -i 's#jb##g' ./build/output
-sed -i 's#src##g' ./build/output
-sed -i 's#samarium##g' ./build/output
+# sed -i 's#src##g' ./build/output
+# sed -i 's#samarium##g' ./build/output
 sed -i 's#CMakeFiles##g' ./build/output
 sed -i 's#.dir##g' ./build/output
 sed -i 's#//##g' ./build/output
--- test/dev.cpp
+++ test/dev.cpp
@@ -13,32 +13,48 @@ using namespace sm::literals;
 
 int main()
 {
-    const auto speed = 2;
+    const auto speed = 4.0;
 
     auto app = App{{.dims = {1800, 900}}};
     app.transform.scale *= 6;
+    app.transform.pos = app.dims().as<f64>() * Vector2{0.1, 0.85};
 
-    auto player     = Particle{};
-    const auto disp = Vector2{3, 3};
-
-    // app.keymap.push_back({Keyboard::Key::Space}, [] { print("Jump"); });
-    auto jumper =
-        Keyboard::EventListener<Keyboard::Event::Down, Keyboard::Key::Space>{[] { print("Jump"); }};
+    auto player     = Particle{.pos = {0.0, 2.0}};
+    const auto disp = Vector2{1.5, 1.5};
+    auto trail      = Trail{200};
+    auto jumper     = Keyboard::OnKeyDown{{Keyboard::Key::Space},
+                                      [&]
+                                      {
+                                          player.vel.y += 15;
+                                          print("Jump");
+                                      }};
 
     const auto update = [&](f64 dt)
     {
         jumper();
-        app.zoom_pan();
+        // app.zoom_pan();
         app.transform.pos.x -= speed * dt * app.transform.scale.x;
+
+        player.acc = Vector2{0.0, -30};
+
+        player.update(dt);
         player.pos.x += speed * dt;
+
+        if (player.pos.y <= 0.0)
+        {
+            player.pos.y = 0;
+            player.vel.y = 0;
+        }
+        trail.push_back(player.pos + disp / 2.0);
     };
 
     const auto draw = [&]
     {
-        app.fill("#141724"_c);
-        app.draw(App::GridLines{.scale = 1.0, .axis_thickness = 0.016, .line_thickness = 0.01});
-        // app.draw(App::GridDots{.scale = 1.0, .thickness = 0.03});
-        app.draw(BoundingBox<f64>::from_centre_width_height(player.pos, disp.x, disp.y),
+        app.fill("#151726"_c);
+        // app.draw(App::GridLines{.scale = 1.0, .line_thickness = 0.01, .axis_thickness = 0.016});
+        app.draw(App::GridDots{.scale = 1.0, .thickness = 0.03});
+        app.draw(trail, "#17ff70"_c, 0.4, 1.0);
+        app.draw(BoundingBox<f64>{player.pos, player.pos + disp},
                  {.fill_color = "#ff6c17"_c, .border_width = 0.1});
         // app.draw(BoundingBox<f64>{{-4, -4}, {4, 5}},
         //          {.border_color = "#fc035a"_c, .border_width = 0.1});
