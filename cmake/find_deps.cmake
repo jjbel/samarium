561d56f82c34c24cdcdbea23ee6f0068b2d6191b
Jai Bellare <jaibellare@gmail.com>
2022-08-01 12:28:26 +0530

    CMake: find_package: check if already found

--- cmake/find_deps.cmake
+++ cmake/find_deps.cmake
@@ -7,15 +7,26 @@ cmake_minimum_required(VERSION 3.15)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
-find_package(SFML 2.5 CONFIG REQUIRED)
-find_package(fmt CONFIG REQUIRED)
-find_package(range-v3 0.11.0 CONFIG REQUIRED)
-find_package(stb CONFIG REQUIRED)
+if(NOT SFML_FOUND)
+    find_package(SFML CONFIG REQUIRED)
+endif()
+
+if(NOT fmt_FOUND)
+    find_package(fmt CONFIG REQUIRED)
+endif()
+
+if(NOT range-v3_FOUND)
+    find_package(range-v3 CONFIG REQUIRED)
+endif()
+
+if(NOT stb_FOUND)
+    find_package(stb CONFIG REQUIRED)
+endif()
 
 function(link_deps target)
     target_link_libraries(${target} PUBLIC fmt::fmt)
     target_link_libraries(${target} PUBLIC range-v3::range-v3)
-    target_link_libraries(${target} PUBLIC sfml-graphics)
+    target_link_libraries(${target} PUBLIC sfml::sfml)
     target_link_libraries(${target} PUBLIC stb::stb)
 
     if(USE_WARNINGS)
