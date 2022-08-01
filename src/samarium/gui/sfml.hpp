ddccb9596043af9333b71c5443a800ce534d41f4
Jai Bellare <jaibellare@gmail.com>
2022-08-01 12:08:55 +0530

    sfml.hpp: add BoundingBox -> Rect

--- src/samarium/gui/sfml.hpp
+++ src/samarium/gui/sfml.hpp
@@ -10,8 +10,9 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
 
-#include "../graphics/Color.hpp"
-#include "../math/Vector2.hpp"
+#include "samarium/graphics/Color.hpp"
+#include "samarium/math/BoundingBox.hpp"
+#include "samarium/math/Vector2.hpp"
 
 namespace sm
 {
@@ -22,6 +23,11 @@ template <typename T> [[nodiscard]] constexpr Vector2_t<T> sfml(sf::Vector2<T> v
 template <typename T> [[nodiscard]] constexpr sf::Vector2<T> sfml(Vector2_t<T> vec) noexcept
 {
     return {vec.x, vec.y};
+}
+
+template <typename T> [[nodiscard]] constexpr sf::Rect<T> sfml(BoundingBox<T> box) noexcept
+{
+    return {box.min.x, box.min.y, box.width(), box.height()};
 }
 
 [[nodiscard]] inline auto sfml(Color color)
