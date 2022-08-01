e67fb3fc3fe72d6d449db9919cc77941ebcd2fcc
Jai Bellare <jaibellare@gmail.com>
2022-08-01 18:48:58 +0530

    file: use tl::expected, add find()

--- src/samarium/util/file.cpp
+++ src/samarium/util/file.cpp
@@ -9,7 +9,6 @@
 #include <filesystem> // for path
 #include <fstream>    // for ifstream, ofstream, basic_ostream::write
 #include <iterator>   // for ifstreambuf_iterator
-#include <optional>   // for optional
 #include <string>     // for string
 
 #include "fpng/fpng.hpp"
@@ -24,17 +23,18 @@
 
 namespace sm::file
 {
-auto read(Text, const std::filesystem::path& file_path) -> std::optional<std::string>
+auto read(Text, const std::filesystem::path& file_path)
+    -> tl::expected<std::string, FileNotFoundError>
 {
     if (std::filesystem::exists(file_path))
     {
         auto ifs = std::ifstream{file_path};
         return {std::string(std::istreambuf_iterator<char>{ifs}, {})};
     }
-    else { return {}; }
+    else { return tl::unexpected<FileNotFoundError>{{}}; }
 }
 
-auto read(const std::filesystem::path& file_path) -> std::optional<std::string>
+auto read(const std::filesystem::path& file_path) -> tl::expected<std::string, FileNotFoundError>
 {
     return read(Text{}, file_path);
 }
@@ -59,5 +59,58 @@ void export_to(Bmp, const Image& image, const std::filesystem::path& file_path)
     stbi_write_bmp(file_path.string().c_str(), static_cast<i32>(image.dims.x),
                    static_cast<i32>(image.dims.y), 4 /* RGBA */,
                    static_cast<const void*>(&image.front()));
+}
+
+
+auto find(const std::string& file_name, const std::filesystem::path& directory)
+    -> tl::expected<std::filesystem::path, FileNotFoundError>
+{
+    for (const auto& dir_entry : std::filesystem::recursive_directory_iterator(directory))
+    {
+        if (dir_entry.is_regular_file() && dir_entry.path().filename() == file_name)
+        {
+            return {std::filesystem::canonical(dir_entry)};
+        }
+    }
+
+    return tl::unexpected<FileNotFoundError>{{}};
+}
+
+auto find(const std::string& file_name, std::span<std::filesystem::path> search_paths)
+    -> tl::expected<std::filesystem::path, FileNotFoundError>
+{
+    for (const auto& path : search_paths)
+    {
+        if (std::filesystem::is_directory(path))
+        {
+            if (const auto found_path = find(file_name, path)) { return {found_path}; }
+        }
+        else if (std::filesystem::is_regular_file(path))
+        {
+            if (std::filesystem::exists(path)) { return {std::filesystem::canonical(path)}; }
+        }
+        else if (!std::filesystem::exists(path)) { continue; }
+    }
+
+    return tl::unexpected<FileNotFoundError>{{}};
+}
+
+auto find(const std::string& file_name, std::initializer_list<std::filesystem::path> search_paths)
+    -> tl::expected<std::filesystem::path, FileNotFoundError>
+{
+    for (const auto& path : search_paths)
+    {
+        if (std::filesystem::is_directory(path))
+        {
+            if (const auto found_path = find(file_name, path)) { return {found_path}; }
+        }
+        else if (std::filesystem::is_regular_file(path))
+        {
+            if (std::filesystem::exists(path)) { return {std::filesystem::canonical(path)}; }
+        }
+        else if (!std::filesystem::exists(path)) { continue; }
+    }
+
+    return tl::unexpected<FileNotFoundError>{{}};
 }
 } // namespace sm::file
--- src/samarium/util/file.hpp
+++ src/samarium/util/file.hpp
@@ -8,13 +8,13 @@
 #pragma once
 
 #include <filesystem>
-#include <optional>
 #include <string>
 
 #include "fpng/fpng.hpp"
+#include "tl/expected.hpp"
 
-#include "../graphics/Image.hpp"
-#include "../util/format.hpp"
+#include "samarium/graphics/Image.hpp"
+#include "samarium/util/format.hpp"
 
 namespace sm::file
 {
@@ -34,20 +34,13 @@ struct Bmp
 {
 };
 
+struct FileNotFoundError
+{
+};
 
-// namespace
-// {
-// auto png_init()
-// {
-//     fpng::fpng_init();
-//     return true;
-// }
-// static const bool b = png_init();
-// } // namespace
-
-
-auto read(Text, const std::filesystem::path& file_path) -> std::optional<std::string>;
-auto read(const std::filesystem::path& file_path) -> std::optional<std::string>;
+auto read(Text, const std::filesystem::path& file_path)
+    -> tl::expected<std::string, FileNotFoundError>;
+auto read(const std::filesystem::path& file_path) -> tl::expected<std::string, FileNotFoundError>;
 
 void export_to(Targa,
                const Image& image,
@@ -65,4 +58,14 @@ inline void export_to(Png,
 void export_to(Bmp,
                const Image& image,
                const std::filesystem::path& file_path = date_time_str() + ".bmp");
+
+auto find(const std::string& file_name,
+          const std::filesystem::path& directory = std::filesystem::current_path())
+    -> tl::expected<std::filesystem::path, FileNotFoundError>;
+
+auto find(const std::string& file_name, std::span<std::filesystem::path> search_paths)
+    -> tl::expected<std::filesystem::path, FileNotFoundError>;
+
+auto find(const std::string& file_name, std::initializer_list<std::filesystem::path> search_paths)
+    -> tl::expected<std::filesystem::path, FileNotFoundError>;
 } // namespace sm::file
