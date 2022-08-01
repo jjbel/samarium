516a70950d88de4e2eac9028c5a11131fe834d67
Jai Bellare <jaibellare@gmail.com>
2022-08-01 12:43:06 +0530

    test_package: specify Image size

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -9,7 +9,7 @@
 
 int main()
 {
-    const auto im = sm::Image{};
+    const auto im = sm::Image{sm::dimsFHD};
     fmt::print(fmt::emphasis::bold, "\nSuccessful installation!\n");
     fmt::print(fmt::emphasis::bold, "Welcome to {}\n", sm::version);
     sm::print("A Vector2:", sm::Vector2{.x = 5, .y = -3});
