cf3218f50089d295dc848f71e1c813b09fa0dae2
Jai Bellare <jaibellare@gmail.com>
2022-09-04 23:35:25 +0530

    dev changes: image write benchmarks

--- test/dev.cpp
+++ test/dev.cpp
@@ -5,9 +5,31 @@
  * Project homepage: https://github.com/strangeQuark1041/samarium
  */
 
+#include "samarium/graphics/Image.hpp"
 #include "samarium/samarium.hpp"
+#include "samarium/util/Stopwatch.hpp"
+#include "samarium/util/file.hpp"
+#include "samarium/util/fpng/fpng.hpp"
+#include <chrono>
+#include <thread>
 
 using namespace sm;
 using namespace sm::literals;
 
-int main() { auto body = RigidBody{}; }
+int main()
+{
+    auto body = RigidBody{};
+    auto im   = expect(file::read_image("/home/jb/Pictures/alphatuari-f1.jpg"));
+
+    auto watch = Stopwatch{};
+    file::write(file::Png{}, im);
+    watch.print();
+
+    watch.reset();
+    file::write(file::Targa{}, im);
+    watch.print();
+
+    watch.reset();
+    file::write(file::Bmp{}, im);
+    watch.print();
+}
