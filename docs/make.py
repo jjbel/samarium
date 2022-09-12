832e9fe250515bf747c577a4b3d3720e4138d6eb
Jai Bellare <jaibellare@gmail.com>
2022-09-12 11:55:27 +0530

    docs: make.py: fix bug

--- docs/make.py
+++ docs/make.py
@@ -4,16 +4,21 @@ from math import modf
 from subprocess import run
 from pathlib import Path
 import time
+from sys import exit
 
-SPHINXOPTS = []
-SPHINXBUILD = "sphinx-build"
 SOURCEDIR = "."
 BUILDDIR = "build/html"
+SPHINXOPTS = []
+SPHINXBUILD = "sphinx-build"
 
 Path(BUILDDIR).mkdir(parents=True, exist_ok=True)
 
 start = time.time()
-run([SPHINXBUILD, "-b", "html", "-j", "auto", SOURCEDIR, BUILDDIR] + SPHINXOPTS)
+run(
+    [SPHINXBUILD, "-b", "html", "-j", "auto", SOURCEDIR, BUILDDIR] + SPHINXOPTS,
+    check=True,
+)
 end = time.time()
+
 millis, seconds = modf(end - start)
-print(f'Built in {int(seconds)}s {int(millis * 1000)}ms')
+print(f"Built in {int(seconds)}s {int(millis * 1000)}ms")
