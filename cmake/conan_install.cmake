491cf2a57263f58f6bdf6cf4584d5b6a2377c530
Jai Bellare <jaibellare@gmail.com>
2022-09-16 08:10:16 +0530

    cmake: conan_install: tests deps

--- cmake/conan_install.cmake
+++ cmake/conan_install.cmake
@@ -5,12 +5,18 @@
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/conan.lock AND RUN_CONAN)
     find_program(CONAN_EXE conan REQUIRED)
 
-    message(STATUS "Installing dependencies... (this may take a few minutes)")
+    set(DEPS_OPTION "")
+
+    if(BUILD_UNIT_TESTS OR BUILD_BENCHMARKS)
+        set(DEPS_OPTION "-o samarium:build_tests=True")
+    endif()
+
+    message(STATUS "Installing dependencies ... (this may take a few seconds)")
     execute_process(
         COMMAND
             ${CONAN_EXE} install . -b missing -if ${CMAKE_CURRENT_BINARY_DIR}
             -pr:b=cmake/clang_${CMAKE_BUILD_TYPE}.profile
-            -pr=cmake/clang_${CMAKE_BUILD_TYPE}.profile
+            -pr=cmake/clang_${CMAKE_BUILD_TYPE}.profile ${DEPS_OPTION}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         OUTPUT_QUIET
     )
