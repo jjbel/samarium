bf1e393a88453d563259b872fe3ceddcd0b17b6f
Jai Bellare <jaibellare@gmail.com>
2022-09-16 08:12:01 +0530

    docs: make.py: improve usage

--- docs/make.py
+++ docs/make.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 from math import modf
+from sys import argv
 from subprocess import run
 from pathlib import Path
 import time
-from sys import exit
 
 SOURCEDIR = "."
 BUILDDIR = "build/html"
 SPHINXOPTS = []
 SPHINXBUILD = "sphinx-build"
+TARGET = argv[1] if len(argv) == 2 else "dirhtml";
 
 Path(BUILDDIR).mkdir(parents=True, exist_ok=True)
 
 start = time.time()
 run(
-    [SPHINXBUILD, "-b", "html", "-j", "auto", SOURCEDIR, BUILDDIR] + SPHINXOPTS,
+    [SPHINXBUILD, "-TEqb", TARGET, "-j", "auto", SOURCEDIR, BUILDDIR] + SPHINXOPTS,
     check=True,
 )
 end = time.time()
