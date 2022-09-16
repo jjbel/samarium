14da597bd14ec098235699bf04775c896d321c23
Jai Bellare <jaibellare@gmail.com>
2022-09-16 08:11:45 +0530

    docs: conf.py: fix bugs

--- docs/conf.py
+++ docs/conf.py
@@ -53,13 +53,11 @@ breathe_default_members = ("members", "undoc-members")
 primary_domain = "cpp"
 highlight_language = "cpp"
 
-ogp_site_url="https://strangequark1041.github.io/samarium/c"
+ogp_site_url = "https://strangequark1041.github.io/samarium/"
 ogp_site_name = "Samarium Docs"
 
 mathjax_path = (
     "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 )
 
-print("Running Doxygen...", end=" ")
 run(["doxygen", "Doxyfile.cfg"], check=True)
-print("done")
