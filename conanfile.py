d9eebfb97628a1af21c2fb72792fc8e0e56a9844
Jai Bellare <jaibellare@gmail.com>
2022-09-28 20:49:15 +0530

    conan: bump catch version

--- conanfile.py
+++ conanfile.py
@@ -36,7 +36,7 @@ class SamariumConan(ConanFile):
         ]
 
         if self.options.build_tests:
-            deps += ["catch2/3.0.1", "benchmark/1.6.1"]
+            deps += ["catch2/3.1.0", "benchmark/1.6.1"]
 
         for dep in deps:
             self.requires(dep)
