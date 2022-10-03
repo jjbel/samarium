a4543bbb7ac1752fafcca11f407fc2fe0d81edd1
Jai Bellare <jaibellare@gmail.com>
2022-10-03 22:59:21 +0530

    cmake: add SAMARIUM_HEADER_ONLY option

--- test/dev.cpp
+++ test/dev.cpp
@@ -5,20 +5,9 @@
  * Project homepage: https://github.com/strangeQuark1041/samarium
  */
 
-#include "samarium/math/Vector2.hpp"
-#define SAMARIUM_HEADER_ONLY
+#include "samarium/samarium.hpp"
 
-// #include "samarium/samarium.hpp"
+using namespace sm;
+using namespace sm::literals;
 
-// using namespace sm;
-// using namespace sm::literals;
-
-int main()
-{
-    auto x = 42;
-    x++;
-    auto y = x;
-    auto v = sm::Vector2{-12, 16};
-    v.x -= -5;
-    v = v.abs();
-}
+int main() {}
