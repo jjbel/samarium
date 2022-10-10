38d8b252f450262fb6c0453f9a0cd4dd95fbca8c
Jai Bellare <jaibellare@gmail.com>
2022-10-10 16:27:20 +0530

    examples: restructure

---
+++ examples/usage/input.cpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Jai Bellare
+ * See <https://opensource.org/licenses/MIT/> or LICENSE.md
+ * Project homepage: https://github.com/strangeQuark1041/samarium
+ */
+
+#include "samarium/samarium.hpp"
+
+using namespace sm;
+using namespace sm::literals;
+
+int main()
+{
+    auto window = Window{{1024, 1024}};
+    auto watch  = Stopwatch{};
+
+    auto action = keyboard::OnKeyPress(*window.handle.get(), {Key::W}, [] { print("Key"); });
+
+    while (window.is_open())
+    {
+        draw::fill("#1c151b"_c);
+        draw::circle(window, {{0.2, 0.3}, 0.4}, {.fill_color = "#fa2844"_c});
+        watch.reset();
+
+        if (Window::resized) { print("Resized"); }
+        if (window.mouse.left) { print("Left"); }
+        if (window.mouse.right) { print("Right"); }
+        if (window.is_key_pressed(Key::Space)) { print("Space"); }
+        action();
+        window.display();
+    }
+}
