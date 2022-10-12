4fa26a4e9bb831d13ab4e8319d5582b591f8b262
Jai Bellare <jaibellare@gmail.com>
2022-10-12 22:48:27 +0530

    core: add inline

---
+++ src/samarium/core/inline.hpp
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Jai Bellare
+ * See <https://opensource.org/licenses/MIT/> or LICENSE.md
+ * Project homepage: https://github.com/strangeQuark1041/samarium
+ */
+
+#ifdef SAMARIUM_HEADER_ONLY
+#define SM_INLINE inline
+#else
+#define SM_INLINE
+#endif
