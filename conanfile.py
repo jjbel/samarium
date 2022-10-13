b976cb798982f8a4a302733bab94a0a9de9146d0
Jai Bellare <jaibellare@gmail.com>
2022-10-13 19:38:52 +0530

    conan: use svector

--- conanfile.py
+++ conanfile.py
@@ -40,6 +40,7 @@ class SamariumConan(ConanFile):
             "tl-function-ref/1.0.0",
             "bshoshany-thread-pool/3.3.0",
             "unordered_dense/1.4.0",
+            "svector/1.0.2",
             "glfw/3.3.8",
             "glm/cci.20220420",
             "glad/0.1.36",
