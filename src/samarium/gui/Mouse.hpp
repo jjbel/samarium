1be8ff410f9f41a8acadb541683785a1454d8536
Jai Bellare <jaibellare@gmail.com>
2022-10-14 23:49:42 +0530

    gui: Mouse: simplify

--- src/samarium/gui/Mouse.hpp
+++ src/samarium/gui/Mouse.hpp
@@ -7,65 +7,22 @@
 
 #pragma once
 
-#include "samarium/core/types.hpp" // for f64
-// #include "samarium/math/Transform.hpp" // for Transform
+#include "samarium/core/types.hpp"   // for f64
 #include "samarium/math/Vector2.hpp" // for Vector2
 
 namespace sm
 {
 struct Mouse
 {
-    enum class Button
-    {
-        Left,
-        Right
-    };
-
     Vector2 pos{};
     Vector2 old_pos{};
     f64 scroll_amount{};
     bool left{};
     bool middle{};
     bool right{};
-
-    // [[nodiscard]] auto apply(Transform transform, Mouse::Button btn = Button::Left) const
-    //     -> Transform;
-
-    // [[nodiscard]] auto vel() const -> Vector2;
 };
 } // namespace sm
 
 #if defined(SAMARIUM_HEADER_ONLY) || defined(SAMARIUM_MOUSE_IMPL)
-
-// #include "SFML/Graphics/RenderWindow.hpp" // for RenderWindow
-// #include "SFML/Window/Mouse.hpp"          // for Mouse
-
-// #include "samarium/gui/sfml.hpp"       // for sfml
-// #include "samarium/math/Transform.hpp" // for Transform
-
-// #include "Mouse.hpp"
-
-// namespace sm
-// {
-// Vector2 Mouse::vel() const { return this->current_pos - this->old_pos; }
-
-// void Mouse::update(const sf::RenderWindow& window)
-// {
-//     this->old_pos     = this->current_pos;
-//     this->current_pos = sfml(sf::Mouse::getPosition(window)).as<f64>();
-//     this->left        = sf::Mouse::isButtonPressed(sf::Mouse::Left);
-//     this->middle      = sf::Mouse::isButtonPressed(sf::Mouse::Middle);
-//     this->right       = sf::Mouse::isButtonPressed(sf::Mouse::Right);
-// }
-
-// Transform Mouse::apply(Transform transform, Mouse::Button btn) const
-// {
-//     if ((btn == Mouse::Button::Left && this->left) || (btn == Mouse::Button::Right &&
-//     this->right))
-//         transform.pos += this->vel();
-
-//     return transform;
-// }
-// } // namespace sm
 
 #endif
