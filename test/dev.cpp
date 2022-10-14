780c7d2e3f05eeac36b8c387595c080e30bd7ecc
Jai Bellare <jaibellare@gmail.com>
2022-10-14 18:03:55 +0530

    dev changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -5,6 +5,7 @@
  * Project homepage: https://github.com/strangeQuark1041/samarium
  */
 
+#include "samarium/gl/gl.hpp"
 #include "samarium/math/Vector2.hpp"
 #include "samarium/samarium.hpp"
 
@@ -13,23 +14,23 @@ using namespace sm::literals;
 
 int main()
 {
-    // auto window = Window{{1024, 1024}};
-    // auto watch  = Stopwatch{};
+    print(gl::version_major, gl::version_minor);
+    auto window = Window{{1024, 1024}};
+    auto watch  = Stopwatch{};
 
-    // auto action = keyboard::OnKeyPress(*window.handle, {Key::W}, [] { print("Key"); });
+    auto action = keyboard::OnKeyPress(*window.handle, {Key::W}, [] { print("Key"); });
 
-    // while (window.is_open())
-    // {
-    //     draw::fill("#1c151b"_c);
-    //     draw::circle(window, {{0.2, 0.3}, 0.4}, {.fill_color = "#fa2844"_c});
-    //     watch.reset();
+    while (window.is_open())
+    {
+        draw::fill("#1c151b"_c);
+        draw::circle(window, {{0.2, 0.3}, 0.4}, {.fill_color = "#fa2844"_c});
+        watch.reset();
 
-    //     if (Window::resized) { print("Resized"); }
-    //     if (window.mouse.left) { print("Left"); }
-    //     if (window.mouse.right) { print("Right"); }
-    //     if (window.is_key_pressed(Key::Space)) { print("Space"); }
-    //     action();
-    //     window.display();
-    // }
-    print(Vector2{2.0, 0.0}.rotated_about(135.0_degrees, {1.0, 0.0}));
+        if (Window::resized) { print("Resized"); }
+        if (window.mouse.left) { print("Left"); }
+        if (window.mouse.right) { print("Right"); }
+        if (window.is_key_pressed(Key::Space)) { print("Space"); }
+        action();
+        window.display();
+    }
 }
