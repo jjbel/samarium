027d4ab76d86d7d0f1ee615f5b4e83fa980b2217
Jai Bellare <jaibellare@gmail.com>
2022-10-27 20:03:55 +0530

    dev changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -1,5 +1,7 @@
 #include "samarium/graphics/colors.hpp"
 #include "samarium/samarium.hpp"
+#include "samarium/util/Stopwatch.hpp"
+#include <fmt/format.h>
 
 using namespace sm;
 using namespace sm::literals;
@@ -9,14 +11,17 @@ int main()
     auto window = Window{{1280, 720}};
     auto text   = expect(
         draw::Text::make("/usr/share/fonts/TTF/Fira Code Regular Nerd Font Complete Mono.ttf"));
+    auto watch = Stopwatch{};
 
     while (window.is_open())
     {
+        watch.reset();
         draw::background("#141414"_c);
         draw::circle(window, {.centre = {}, .radius = .3}, {.fill_color = colors::red});
-        // text(window, "Bezier Curves", {200.0F, 300.0F}, 1.0F, colors::ivory);
-        text(window, "libsamarium", {}, 1.0F, colors::ivory);
+        text(window, "Bezier Curves", {0.2000F, 0.3000F}, 1.0F, colors::ivory);
+        text(window, fmt::format("{:3.2} ms", watch.seconds() * 1000), {}, 1.0F, colors::ivory);
         window.display();
     }
+
     print("DoNnnnn");
 }
