341017232b879cfe519110a9c82841ccd2cbf977
Jai Bellare <jaibellare@gmail.com>
2022-11-16 21:06:31 +0530

    add lite.hpp

---
+++ src/samarium/lite.hpp
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Jai Bellare
+ * See <https://opensource.org/licenses/MIT/> or LICENSE.md
+ * Project homepage: https://github.com/strangeQuark1041/samarium
+ */
+
+#pragma once
+
+#include "samarium/core.hpp"
+#include "samarium/graphics.hpp"
+#include "samarium/gui.hpp"
+#include "samarium/math.hpp"
+#include "samarium/physics.hpp"
