52e0a6a4f3efc0e26a1851576d6d03311a2255ae
Jai Bellare <jaibellare@gmail.com>
2022-11-18 00:53:43 +0530

    dev changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -5,27 +5,33 @@
  * Project homepage: https://github.com/strangeQuark1041/samarium
  */
 
-#include "samarium/gl/draw.hpp"
+#include "samarium/lite.hpp"
+
 #include "samarium/graphics/colors.hpp"
-#include "samarium/math/interp.hpp"
-#include "samarium/samarium.hpp"
+#include "samarium/util/Stopwatch.hpp"
 
 using namespace sm;
 using namespace sm::literals;
 
 auto main() -> i32
 {
-    auto window = Window{{1280, 720}};
-    window.view.scale *= 0.3;
+    auto window = Window{{1800, 900}};
+    window.view.scale /= 10.0;
     auto watch       = Stopwatch{};
     const auto scale = window.aspect_vector_min().length() * window.view.scale.length();
     print(scale, window.aspect_vector_max(), window.view.scale);
 
+    const auto circle = [&](Vector2 pos)
+    { draw::circle(window, {.centre = pos, .radius = .15}, {.fill_color = colors::red}); };
+
     while (window.is_open())
     {
         watch.reset();
-        draw::background("#141414"_c);
-        draw::circle(window, {.centre = {}, .radius = .3}, {.fill_color = colors::red});
+        draw::background("#101113"_c);
+
+        circle({});
+        draw::grid_dots(window);
+
         const auto vec =
             window.view.apply_inverse(interp::map_range<Vector2>(
                 window.mouse.pos, {{0, 0}, window.dims.cast<f64>()}, {{-1, -1}, {1, 1}})) *
