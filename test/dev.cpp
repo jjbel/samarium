21fa48c6fe2102ea9395ccee93fbff473660a541
Jai Bellare <jaibellare@gmail.com>
2022-11-25 12:25:54 +0530

    dev changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -5,49 +5,78 @@
  * Project homepage: https://github.com/strangeQuark1041/samarium
  */
 
-#include "samarium/lite.hpp"
-
-#include "samarium/graphics/colors.hpp"
-#include "samarium/util/Stopwatch.hpp"
+#include "samarium/samarium.hpp"
 
 using namespace sm;
 using namespace sm::literals;
 
 auto main() -> i32
 {
+    const auto particle_count    = u64{50000};
+    const auto hash_grid_spacing = 0.2;
+
     auto window = Window{{1800, 900}};
-    window.view.scale /= 10.0;
-    auto watch = Stopwatch{};
 
-    const auto circle = [&](Vector2 pos)
-    { draw::circle(window, {.centre = pos, .radius = .5}, {.fill_color = colors::red}); };
-
-    auto counter = 0;
-    auto total   = 0.0;
-
-    while (window.is_open())
+    auto random   = RandomGenerator{1024, RandomMode::NonDeterministic};
+    auto viewport = window.viewport();
+    auto ps       = ParticleSystem::generate(particle_count,
+                                             [&](u64 /* index */)
+                                             {
+                                           return Particle{.pos = random.vector(viewport) * 0.95,
+                                                                 .vel = random.polar_vector({0.0, 2.0}),
+                                                                 .radius = .03,
+                                                                 .mass   = 1.0};
+                                       });
+    // auto ps         = ParticleSystem{2};
+    // ps.particles[0] = Particle{.pos = {-8, 0}, .vel = {8, 0}, .radius = 1.4, .mass = 1.0};
+    // ps.particles[1] = Particle{.pos = {8, 0}, .vel = {0, 0}, .radius = 1.8, .mass = 1.0};
+    auto watch  = Stopwatch{};
+    auto update = [&]
     {
-        draw::background("#101113"_c);
+        const auto dt = 1.0 / 60.0;
 
-        circle({});
+        ps.for_each(
+            [&](Particle& particle)
+            {
+                for (const auto& wall : viewport.line_segments())
+                {
+                    phys::collide(particle, wall, dt, 1.0);
+                }
+            });
 
         watch.reset();
-        draw::grid_dots(window);
-        total += watch.seconds();
-        counter++;
+        const auto [count1, count2] = ps.self_collision(1.0, hash_grid_spacing);
+        const auto time             = watch.seconds();
+        fmt::print("{:4}/{:6} collisions in {:3.2}ms\n", count1, count2, time * 1000.0);
 
+        ps.update(dt);
+    };
 
-        const auto vec =
-            window.view.apply_inverse(interp::map_range<Vector2>(
-                window.mouse.pos, {{0, 0}, window.dims.cast<f64>()}, {{-1, -1}, {1, 1}})) *
-            Vector2{1, -1};
+    auto draw = [&]
+    {
+        // zoom_pan(window);
+        // print("mouse pos:", window.mouse.pos - window.mouse.old_pos);
+        draw::background("#020407"_c);
+        draw::grid_lines(window, {.spacing   = hash_grid_spacing,
+                                  .color     = "#eeeeee"_c.with_multiplied_alpha(0.1),
+                                  .thickness = 0.04});
 
+        for (const auto& i : viewport.line_segments())
+        {
+            draw::line_segment(window, i, "#eeeeee"_c, 0.2);
+        }
 
-        draw::line(window, {vec, {2.0, 1.0}}, colors::goldenrod, 0.1);
-        window.display();
+        for (const auto& i : ps)
+        {
+            // TODO gl error for polygon
+            // draw::circle(window, {i.pos, i.radius},
+            //              {.fill_color   = "#5640ff"_c.with_multiplied_alpha(0.2),
+            //               .border_color = {255, 30, 30, 255},
+            //               .border_width = 1});
+            draw::circle(window, {i.pos, i.radius},
+                         {.fill_color = "#5640ff"_c.with_multiplied_alpha(0.4)});
+        }
+    };
 
-        if (counter > 400) { break; }
-    }
-
-    fmt::print("Average: {:3.3f}ms\n", total / counter * 1000.0);
+    run(window, update, draw);
 }
