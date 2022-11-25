291e0e68acd2c70393d23545e785911aefc4e6d3
Jai Bellare <jaibellare@gmail.com>
2022-11-25 12:25:34 +0530

    ParticleSystem: make header-only

--- src/samarium/physics/ParticleSystem.hpp
+++ src/samarium/physics/ParticleSystem.hpp
@@ -6,12 +6,14 @@
  */
 
 #pragma once
+
 #include <memory> // for allocator_trai...
 #include <span>   // for span
 #include <vector> // for vector
 
 #include "BS_thread_pool.hpp"                         // for multi_future
-#include "range/v3/algorithm/for_each.hpp"            // for for_each, for_...
+#include "range/v3/algorithm/for_each.hpp"            // for for_each
+#include "range/v3/algorithm/sort.hpp"                // for sort
 #include "range/v3/functional/identity.hpp"           // for identity
 #include "range/v3/iterator/basic_iterator.hpp"       // for basic_iterator
 #include "range/v3/iterator/unreachable_sentinel.hpp" // for operator==
@@ -28,6 +30,7 @@
 #include "samarium/util/FunctionRef.hpp" // for FunctionRef
 #include "samarium/util/HashGrid.hpp"    // for HashGrid
 #include "samarium/util/ThreadPool.hpp"  // for ThreadPool
+#include "samarium/util/util.hpp"        // for project_view
 
 #include "collision.hpp" // for collide
 
@@ -59,89 +62,82 @@ struct ParticleSystem
         // https://stackoverflow.com/a/70808634/17100530
     }
 
-    void update(f64 time_delta = 1.0) noexcept;
-    void update(ThreadPool& thread_pool, f64 time_delta = 1.0) noexcept;
-
-    void apply_force(Vector2 force) noexcept;
-    void apply_forces(std::span<Vector2> forces) noexcept;
-
-    void for_each(FunctionRef<void(Particle&)> function);
-
-    template <usize MaxParticlesInCell = 32>
-    void self_collision(f64 damping = 1.0, f64 cell_size = 2.0) noexcept
+    void update(f64 time_delta = 1.0) noexcept
     {
-        auto hash_grid = HashGrid<u64, MaxParticlesInCell>{cell_size};
+        ranges::for_each(particles,
+                         [time_delta](Particle& particle) { particle.update(time_delta); });
+    }
+
+    void update(ThreadPool& thread_pool, f64 time_delta = 1.0) noexcept
+    {
+        const auto job = [&](auto min, auto max)
+        {
+            for (auto i : range(min, max)) { particles[i].update(time_delta); }
+        };
+
+        thread_pool.parallelize_loop(0UL, particles.size(), job, thread_pool.get_thread_count())
+            .wait();
+    }
+
+    void apply_force(Vector2 force) noexcept
+    {
+        ranges::for_each(particles, [force](Particle& particle) { particle.apply_force(force); });
+    }
+
+    void apply_forces(std::span<Vector2> forces) noexcept
+    {
+        for (auto i : range(particles.size())) { particles[i].apply_force(forces[i]); }
+    }
+
+    void for_each(FunctionRef<void(Particle&)> function) { ranges::for_each(particles, function); }
+
+    /**
+     * @brief               Collide the particles with themselves
+     *
+     * @tparam CellCapacity Max particles in one cell of the hash grid
+     * @param  damping      Coefficient of restitution
+     * @param  cell_size    Size of cell of hash grid
+     * @return Dimensions   [collisions, pairs checked]
+     */
+    template <u64 CellCapacity = 32>
+    [[maybe_unused]] auto self_collision(f64 damping = 1.0, f64 cell_size = 0.5)
+    {
+        HashGrid<u32, CellCapacity> hash_grid{cell_size};
         hash_grid.map.reserve(particles.size());
         for (auto i : range(particles.size())) { hash_grid.insert(particles[i].pos, i); }
 
-        usize count = 0;
+        auto count1 = u32{};
+        auto count2 = u32{};
         for (auto i : range(particles.size()))
         {
+            // Slow: for (auto j : range(particles.size()))
+            // TODO bottleneck is still here, not in the actual collision
             for (auto j : hash_grid.neighbors(particles[i].pos))
             {
-                if (i != j) { count += phys::collide(particles[i], particles[j], damping); }
+                // we're looping through ordered pairs, so avoid colliding each pair twice
+                if (i < j)
+                {
+                    count1 += phys::collide(particles[i], particles[j], damping);
+                    count2++;
+                }
             }
         }
-        // print(count);
+        return Dimensions::make(count1, count2);
     }
 
-    auto operator[](u64 index) noexcept { return particles[index]; }
-    auto operator[](u64 index) const noexcept { return particles[index]; }
+    [[nodiscard]] auto operator[](u64 index) noexcept { return particles[index]; }
+    [[nodiscard]] auto operator[](u64 index) const noexcept { return particles[index]; }
 
-    auto begin() noexcept { return particles.begin(); }
-    auto end() noexcept { return particles.end(); }
+    [[nodiscard]] auto begin() noexcept { return particles.begin(); }
+    [[nodiscard]] auto end() noexcept { return particles.end(); }
 
-    auto begin() const noexcept { return particles.cbegin(); }
-    auto end() const noexcept { return particles.cend(); }
+    [[nodiscard]] auto begin() const noexcept { return particles.cbegin(); }
+    [[nodiscard]] auto end() const noexcept { return particles.cend(); }
 
-    auto cbegin() const noexcept { return particles.cbegin(); }
-    auto cend() const noexcept { return particles.cend(); }
+    [[nodiscard]] auto cbegin() const noexcept { return particles.cbegin(); }
+    [[nodiscard]] auto cend() const noexcept { return particles.cend(); }
 
-    auto size() const noexcept { return particles.size(); }
-    auto empty() const noexcept { return particles.empty(); }
+    [[nodiscard]] auto size() const noexcept { return particles.size(); }
+    [[nodiscard]] auto empty() const noexcept { return particles.empty(); }
 };
 } // namespace sm
-
-#if defined(SAMARIUM_HEADER_ONLY) || defined(SAMARIUM_PARTICLE_SYSTEM_IMPL)
-
-#include "range/v3/algorithm/for_each.hpp" // for for_each
-#include "range/v3/algorithm/sort.hpp"     // for sort
-
-#include "samarium/math/Extents.hpp"     // for Extents, Extents<>::Iter...
-#include "samarium/physics/Particle.hpp" // for Particle
-#include "samarium/util/FunctionRef.hpp" // for FunctionRef
-#include "samarium/util/util.hpp"        // for project_view
-
-namespace sm
-{
-void ParticleSystem::update(f64 time_delta) noexcept
-{
-    ranges::for_each(particles, [time_delta](Particle& particle) { particle.update(time_delta); });
-}
-
-void ParticleSystem::update(ThreadPool& thread_pool, f64 time_delta) noexcept
-{
-    const auto job = [&](auto min, auto max)
-    {
-        for (auto i : range(min, max)) { particles[i].update(time_delta); }
-    };
-
-    thread_pool.parallelize_loop(0UL, particles.size(), job, thread_pool.get_thread_count()).wait();
-}
-
-void ParticleSystem::apply_force(Vector2 force) noexcept
-{
-    ranges::for_each(particles, [force](Particle& particle) { particle.apply_force(force); });
-}
-
-void ParticleSystem::apply_forces(std::span<Vector2> forces) noexcept
-{
-    for (auto i : range(particles.size())) { particles[i].apply_force(forces[i]); }
-}
-
-void ParticleSystem::for_each(FunctionRef<void(Particle&)> function)
-{
-    ranges::for_each(particles, function);
-}
-}; // namespace sm
-#endif
