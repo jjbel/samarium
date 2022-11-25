08a3b4a2f2bf6a116ed429d004cd70139b2a77ce
Jai Bellare <jaibellare@gmail.com>
2022-11-25 12:23:06 +0530

    collision: refactor Particle-Particle

--- src/samarium/physics/collision.hpp
+++ src/samarium/physics/collision.hpp
@@ -16,6 +16,8 @@
 
 #include "Particle.hpp" // for Particle
 
+#include "samarium/util/print.hpp"
+
 namespace sm::phys
 {
 [[nodiscard]] auto did_collide(const Particle& p1, const Particle& p2) -> bool;
@@ -39,36 +41,45 @@ namespace sm::phys
 {
 [[nodiscard]] auto did_collide(const Particle& p1, const Particle& p2) -> bool
 {
-    return math::distance(p1.pos, p2.pos) <= p1.radius + p2.radius;
+    return math::distance(p1.pos, p2.pos) < p1.radius + p2.radius;
 }
+
+namespace detail
+{
+[[nodiscard]] constexpr auto
+solve_collision(f64& vel_left, f64& vel_right, f64 mass_left, f64 mass_right, f64 damping)
+{
+    const auto delta = damping * (vel_left - vel_right);
+    vel_left  = (mass_left * vel_left + mass_right * vel_right - delta) / (mass_left + mass_right);
+    vel_right = vel_left + delta;
+}
+} // namespace detail
 
 [[maybe_unused]] auto collide(Particle& p1, Particle& p2, f64 damping) -> bool
 {
     // https://strangequark1041.github.io/samarium/physics/two-particle-collision
-
     if (!did_collide(p1, p2)) { return false; }
 
-    const auto angle_of_impact = (p2.pos - p1.pos).angle();
+    const auto angle_of_impact = Vector2::angle_between(p1.pos, p2.pos);
 
-    p1.pos.rotate(-angle_of_impact);
+    // ensure p1 is on the left
+    const auto swap_left_right =
+        p1.pos.rotated(-angle_of_impact).x > p2.pos.rotated(-angle_of_impact).x;
+
     p1.vel.rotate(-angle_of_impact);
-    p2.pos.rotate(-angle_of_impact);
     p2.vel.rotate(-angle_of_impact);
 
-    const auto swap_left_right = p1.pos.x > p2.pos.x;
     if (swap_left_right) { std::swap(p1, p2); }
 
-    const auto delta = damping * (p1.vel.x - p2.vel.x);
-    p1.vel.x         = (p1.mass * p1.vel.x + p2.mass * p2.vel.x - delta) / (p1.mass + p2.mass);
-    p2.vel.x         = p1.vel.x + delta;
+    const auto did_collide = p2.vel.x <= p1.vel.x;
+    if (did_collide) { detail::solve_collision(p1.vel.x, p2.vel.x, p1.mass, p2.mass, damping); }
 
+    // cleanup:
     if (swap_left_right) { std::swap(p1, p2); }
-    p1.pos.rotate(angle_of_impact);
     p1.vel.rotate(angle_of_impact);
-    p2.pos.rotate(angle_of_impact);
     p2.vel.rotate(angle_of_impact);
 
-    return true;
+    return did_collide;
 }
 
 void collide(Particle& current, const LineSegment& l, f64 dt, f64 damping, f64 friction)
