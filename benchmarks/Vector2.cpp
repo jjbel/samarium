4781ec9983f8d907b3dec6a478c6519fa0cb174f
Jai Bellare <jaibellare@gmail.com>
2022-12-05 00:23:22 +0530

    benchmarks: Vector2: add more

--- benchmarks/Vector2.cpp
+++ benchmarks/Vector2.cpp
@@ -13,6 +13,24 @@
 
 using namespace sm;
 
+static void Vector2_length(benchmark::State& state)
+{
+    auto vec  = Vector2{1.0, 0.0};
+    auto rand = RandomGenerator{};
+
+    for (auto _ : state) { benchmark::DoNotOptimize(vec.length()); }
+    state.SetItemsProcessed(state.iterations());
+}
+
+static void Vector2_angle(benchmark::State& state)
+{
+    auto vec  = Vector2{1.0, 0.0};
+    auto rand = RandomGenerator{};
+
+    for (auto _ : state) { benchmark::DoNotOptimize(vec.angle()); }
+    state.SetItemsProcessed(state.iterations());
+}
+
 static void Vector2_rotate(benchmark::State& state)
 {
     auto vec  = Vector2{1.0, 0.0};
@@ -25,4 +43,19 @@ static void Vector2_rotate(benchmark::State& state)
     state.SetItemsProcessed(state.iterations());
 }
 
+static void Vector2_set_angle(benchmark::State& state)
+{
+    auto vec  = Vector2{1.0, 0.0};
+    auto rand = RandomGenerator{};
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize((vec.set_angle(rand.range<f64>({0.0, math::two_pi})), 1));
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+
+BENCHMARK(Vector2_length);
+BENCHMARK(Vector2_angle);
 BENCHMARK(Vector2_rotate);
+BENCHMARK(Vector2_set_angle);
