297fcc80e5751a0aca8361737568cb44bd5d82f0
Jai Bellare <jaibellare@gmail.com>
2022-12-05 19:47:59 +0530

    cmake: run_after_build: add docstring

--- cmake/run_after_build.cmake
+++ cmake/run_after_build.cmake
@@ -1,3 +1,17 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022 Jai Bellare
+# See <https://opensource.org/licenses/MIT/> or LICENSE.md
+# Project homepage: <https://github.com/strangeQuark1041/samarium>
+
+# Run the command ${RUN_AFTER_BUILD}
+# after building ${RUN_AFTER_BUILD_DEPENDS}
+# creates the dummy file running_${RUN_AFTER_BUILD_DEPENDS}.txt
+
+# for example
+# set RUN_AFTER_BUILD to "test/samarium_tests" and
+# RUN_AFTER_BUILD_DEPENDS to samarium_tests
+# to run samarium_tests after building it
+
 if(RUN_AFTER_BUILD)
     set(RUN_OUTPUT "${CMAKE_BINARY_DIR}/running_${RUN_AFTER_BUILD_DEPENDS}.txt")
     add_custom_command(
