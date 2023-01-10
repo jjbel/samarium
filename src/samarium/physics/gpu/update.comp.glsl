1774386b3efe90365c6991163a5bc1aedd634529
Jai Bellare <jaibellare@gmail.com>
2023-01-10 20:09:36 +0530

    physics: gpu: update.comp.glsl: temp fix

--- src/samarium/physics/gpu/update.comp.glsl
+++ src/samarium/physics/gpu/update.comp.glsl
@@ -11,10 +11,12 @@ layout(std430, binding = 0) buffer particles_buffer {
 
 void main() {
    uint pos = gl_GlobalInvocationID.x; // get position to read/write data from
+
 //    Particle particle = Particle(particles[pos]);
-//    particle.vel += particle.acc;
-//    particle.pos += particle.vel;
+//    particle.vel += particle.acc * delta_time;
+//    particle.pos += particle.vel * delta_time;
 //    particles[pos] = particle;
-    particles[pos].pos = vec2(122.0, 123.0);
+
+    particles[pos].pos = vec2(2.0, 3.0);
 }
 )glsl"
