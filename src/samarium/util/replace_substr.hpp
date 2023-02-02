1296e809efc615cd8bf3adad1335fc1a8b23d32d
Jai Bellare <jaibellare@gmail.com>
2023-02-03 00:00:23 +0530

    util: add replace_substr

---
+++ src/samarium/util/replace_substr.hpp
@@ -0,0 +1,24 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Jai Bellare
+ * See <https://opensource.org/licenses/MIT/> or LICENSE.md
+ * Project homepage: https://github.com/strangeQuark1041/samarium
+ */
+
+#include <string>
+#include <string_view>
+
+namespace sm::util
+{
+auto replace_substr_inplace(std::string& source, std::string_view old_str, std::string_view new_str)
+{
+    source.replace(source.find(old_str), old_str.size(), new_str);
+}
+
+auto replace_substr(std::string_view source, std::string_view old_str, std::string_view new_str)
+{
+    auto copy = std::string(source);
+    copy.replace(copy.find(old_str), old_str.size(), new_str);
+    return copy;
+}
+} // namespace sm::util
