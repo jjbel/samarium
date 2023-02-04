9741aadd76a3814b3d63921fdfde23bb60478d0f
Jai Bellare <jaibellare@gmail.com>
2023-02-04 12:24:21 +0530

    gl: shaders: polyline: naming

--- src/samarium/gl/shaders/polyline.vert.glsl
+++ src/samarium/gl/shaders/polyline.vert.glsl
@@ -1,7 +1,7 @@
 R"glsl(
 #version 460
 
-layout(std430, binding = 0) buffer TVertex { vec2 vertex[]; };
+layout(std430, binding = 0) buffer ssbo { vec2 vertices[]; };
 
 uniform mat4 view;
 uniform vec2 screen_dims;
@@ -9,13 +9,13 @@ uniform float thickness;
 
 void main()
 {
-    int line_i = gl_VertexID / 6;
-    int tri_i  = gl_VertexID % 6;
+    int line_index = gl_VertexID / 6;
+    int tri_index  = gl_VertexID % 6;
 
     vec4 va[4];
     for (int i = 0; i < 4; ++i)
     {
-        vec4 pos = view * vec4(vertex[line_i + i], 0.0, 1.0);
+        vec4 pos = view * vec4(vertices[line_index + i], 0.0, 1.0);
         pos.xy   = (pos.xy + 1.0) * 0.5 * screen_dims;
         va[i]    = pos;
     }
@@ -24,13 +24,13 @@ void main()
     vec2 nv_line = vec2(-v_line.y, v_line.x);
 
     vec4 pos;
-    if (tri_i == 0 || tri_i == 1 || tri_i == 3)
+    if (tri_index == 0 || tri_index == 1 || tri_index == 3)
     {
         vec2 v_pred  = normalize(va[1].xy - va[0].xy);
         vec2 v_miter = normalize(nv_line + vec2(-v_pred.y, v_pred.x));
 
         pos = va[1];
-        pos.xy += v_miter * thickness * (tri_i == 1 ? -0.5 : 0.5) / dot(v_miter, nv_line);
+        pos.xy += v_miter * thickness * (tri_index == 1 ? -0.5 : 0.5) / dot(v_miter, nv_line);
     }
     else
     {
@@ -38,7 +38,7 @@ void main()
         vec2 v_miter = normalize(nv_line + vec2(-v_succ.y, v_succ.x));
 
         pos = va[2];
-        pos.xy += v_miter * thickness * (tri_i == 5 ? 0.5 : -0.5) / dot(v_miter, nv_line);
+        pos.xy += v_miter * thickness * (tri_index == 5 ? 0.5 : -0.5) / dot(v_miter, nv_line);
     }
 
     pos.xy = pos.xy / screen_dims * 2.0 - 1.0;
