80f56b136b272314d4ae28b8e904135459d01114
Jai Bellare <jaibellare@gmail.com>
2023-02-04 12:24:36 +0530

    draw: trail: ignore when size < 2

--- src/samarium/gl/draw/trail.hpp
+++ src/samarium/gl/draw/trail.hpp
@@ -30,6 +30,7 @@ namespace sm::draw
 {
 SM_INLINE void trail(Window& window, const Trail& trail, Color color, f32 thickness)
 {
+    if (trail.size() < 2) { return; }
     const auto points = trail.trail |
                         ranges::views::transform([](Vector2 vec) { return vec.cast<f32>(); }) |
                         ranges::to<std::vector>;
