27a9c313ca064e0d9ade63df66b4a54eec98968b
Jai Bellare <jaibellare@gmail.com>
2023-02-04 12:25:20 +0530

    dev changes

--- test/dev.cpp
+++ test/dev.cpp
@@ -3,35 +3,40 @@
 using namespace sm;
 using namespace sm::literals;
 
+struct ParametricFunction
+{
+    f64 a{1};
+    f64 b{1};
+    f64 c{1};
+    f64 d{1};
+    f64 scale{1};
+
+    constexpr auto operator()(f64 t) const
+    {
+        return scale * Vector2{std::cos(a * t) - math::power<3>(std::cos(b * t)),
+                               std::sin(c * t) - math::power<3>(std::sin(d * t))};
+    }
+};
+
 auto main() -> i32
 {
-    auto window = Window{{{1800, 900}}};
-    auto ps     = gpu::ParticleSystem{u64(1) << u64(22), Particle<f32>{.pos{0, 0}, .vel{1, 1}}, 16};
-    auto rand   = RandomGenerator{};
-    for (auto& i : ps.particles.data)
-    {
-        i.pos    = rand.vector(window.viewport()).cast<f32>() / 2.0F;
-        i.vel    = rand.polar_vector({0, 4}).cast<f32>();
-        i.radius = 0.1F;
-    }
-
-    auto watch = Stopwatch{};
+    auto window      = Window{{{1800, 900}}};
+    auto fn          = ParametricFunction{1, 40, 1, 40, 5};
+    auto time        = 0.0;
+    const auto speed = 1e-3;
+    auto trail       = Trail{20000};
 
     const auto update = [&]
     {
-        ps.update();
-
-        draw::background("#111114"_c);
-        draw::grid_lines(window, {.spacing = 1, .color{255, 255, 255, 20}, .thickness = 0.03F});
-
-        ps.draw(window, "#2050d6"_c.with_multiplied_alpha(0.4), 0.4F, 3);
+        draw::background("#ffebe2"_c);
+        draw::grid_lines(
+            window,
+            {.spacing = 1, .color = "#0a126a"_c.with_multiplied_alpha(0.15), .thickness = 0.03F});
+        const auto pos = fn(time);
+        trail.push_back(pos);
+        draw::trail(window, trail, "#0a126a"_c.with_multiplied_alpha(0.8), 2);
+        time += speed;
     };
 
-    while (window.is_open())
-    {
-        watch.reset();
-        window.display();
-        update();
-        watch.print();
-    }
+    run(window, update);
 }
