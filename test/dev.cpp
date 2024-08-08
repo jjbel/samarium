45a20ddc68ebeb591da26054403d67bda57bcca1
jjbel <jaibellare@gmail.com>
2024-08-08 13:46:39 +0530

    dev: formatting

--- test/dev.cpp
+++ test/dev.cpp
@@ -1,11 +1,8 @@
 #include "samarium/graphics/colors.hpp"
 #include "samarium/samarium.hpp"
-
 #include "turtle.hpp"
 
-
 using namespace sm;
-using namespace sm::literals;
 
 static auto turtle = Turtle{};
 
@@ -18,11 +15,8 @@ int main()
     {
         draw::background(colors::white);
         turtle.draw(window);
-
-        // turtle.pos.x += 0.001;
-        // turtle.pos.y -= 0.001;
         turtle.angle += 0.0005;
-        turtle.forward(0.03);
+        turtle.forward(0.06);
     };
 
     run(window, update);
