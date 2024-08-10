ac2babe6d62f5971163c43ae145924ac44f872ec
jjbel <jaibellare@gmail.com>
2024-08-11 00:31:28 +0530

    turtle: implement left, right, forward as globals

--- test/dev.cpp
+++ test/dev.cpp
@@ -1,23 +1,14 @@
-#include "samarium/graphics/colors.hpp"
-#include "samarium/samarium.hpp"
 #include "turtle.hpp"
 
-using namespace sm;
-
-static auto turtle = Turtle{};
-
-int main()
+main_program
 {
-    auto window       = Window{{{500, 500}, "Turtle Sim", false}};
-    window.view.scale = Vector2::combine(1.0 / 250.0);
+    int counter = 0;
 
-    const auto update = [&]
+    for (int i = 0; i < 200; i++)
     {
-        draw::background(colors::white);
-        turtle.draw(window);
-        turtle.angle += 0.0005;
-        turtle.forward(0.06);
-    };
+        left(3);
+        forward(6);
+    }
 
-    run(window, update);
+    getClick();
 }
--- test/turtle.hpp
+++ test/turtle.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "samarium/graphics/colors.hpp"
 #include "samarium/samarium.hpp"
 
 namespace sm
@@ -11,6 +12,17 @@ struct Turtle
     f32 angle                   = 0.0;
     static constexpr float size = 20;
     bool pen                    = true;
+    std::vector<LineSegment> segments{};
+
+    Window window;
+
+    Turtle(const WindowConfig& config = {{500, 500}, "Turtle Sim", false}) : window{config}
+    {
+        //  if window is 500x500 scale to pixel coords
+        // 2.0 coz actually need to divide by 250
+        // since default coords go from -1 to 1
+        this->window.view.scale = Vector2(2.0, 2.0) / config.dims.cast<f64>();
+    }
 
     std::vector<Vector2f> tri()
     {
@@ -19,17 +31,63 @@ struct Turtle
                 pos + disp.rotated(-math::pi * 0.5)};
     }
 
-    void draw(Window& window)
+    void draw(f32 thickness = 3.0F)
     {
-        draw::circle(window, {this->pos.cast<f64>(), 2}, {colors::red});
+        for (const auto& seg : this->segments)
+        {
+            draw::line_segment(this->window, seg, Color{0, 0, 0}, thickness);
+        }
+
+
+        draw::circle(this->window, {this->pos.cast<f64>(), 2}, {colors::red});
         // not drawing the circle gives a garbage triangle?
 
-        draw::polygon(window, this->tri(), ShapeColor(colors::transparent, colors::red, 4),
-                      window.view);
+        draw::polygon(this->window, this->tri(), ShapeColor(colors::red, colors::red, 4),
+                      this->window.view);
     }
+
+    void display()
+    {
+        this->draw();
+        this->window.display();
+        draw::background(colors::white);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    // ----------------------------------------- API :
 
     void left(f32 degrees) { this->angle += math::to_radians(degrees); }
     void right(f32 degrees) { this->angle -= math::to_radians(degrees); }
-    void forward(f32 distance) { this->pos += Vector2f::from_polar({distance, this->angle}); }
+
+    void forward(f32 distance)
+    {
+        const auto old_pos = this->pos;
+        this->pos += Vector2f::from_polar({distance, this->angle});
+        this->segments.push_back(LineSegment{old_pos.cast<f64>(), this->pos.cast<f64>()});
+
+        this->display();
+    }
+
+    void getClick()
+    {
+        while (!this->window.mouse.left && this->window.is_open())
+        {
+            this->display();
+            std::cout << this->window.mouse.left << std::endl;
+        }
+    }
 };
 } // namespace sm
+
+#define main_program int main()
+
+using namespace std;
+using namespace sm;
+
+static auto turtle = Turtle{};
+
+void left(f32 degrees) { turtle.left(degrees); }
+void right(f32 degrees) { turtle.right(degrees); }
+void forward(f32 distance) { turtle.forward(distance); }
+void getClick() { turtle.getClick(); }
