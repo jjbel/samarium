b219d3533e9647ee1a57554d9f84cc5a6f964357
jjbel <jaibellare@gmail.com>
2024-08-16 12:56:01 +0530

    scripts: bootstrap: install correct conan version

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -9,17 +9,18 @@ try:
     print('conan already installed.')
 except ModuleNotFoundError:
     print('installing conan...')
-    run(['pip', 'install', 'conan'], check=True, stdout=PIPE)
+    run('pip install --force-reinstall -v "conan==1.64"', shell=True, check=True, stdout=PIPE)
     print('done')
 
+    # TODO test this on CI
     print('adding conan default profile...')
     run(['conan', 'profile', 'new', 'default', '--detect'],  stdout=PIPE)
     print('done')
 
-chdir(Path(__file__).parent.parent)
+chdir(Path(__file__).parent.parent.parent)
 
 print('\ninstalling samarium (this may take some time to build dependencies)')
-run(['conan', 'create', '.',
+run(['conan', 'create', 'samarium',
     '-pr:b=default', '-b', 'missing'], check=True, stdout=PIPE)
 
 print('\ncloning samarium_example into ./samarium_example')
@@ -33,4 +34,8 @@ run(['cmake', '--preset=default'], check=True)
 print('\nbuilding example with "cmake --build --preset=default"')
 run(['cmake', '--build', '--preset=default'], check=True)
 
-print('\nExample ran successfully! Now check out the examples in the examples directory.')
+# TODO only on windows
+print('\nrunning turtle')
+run(['run', 'turtle'], check=True)
+
+print('\nExample ran successfully! Now check out the examples in the samarium/examples directory.')
