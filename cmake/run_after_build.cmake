6997df7c2b84ce3fdc9f9468d13a6f6491bec0d6
jjbel <jaibellare@gmail.com>
2024-08-30 12:23:06 +0530

    cmake: run_after_build: add todo

--- cmake/run_after_build.cmake
+++ cmake/run_after_build.cmake
@@ -13,14 +13,20 @@
 # to run samarium_tests after building it
 
 if(RUN_AFTER_BUILD)
-    set(RUN_OUTPUT "${CMAKE_BINARY_DIR}/running_${RUN_AFTER_BUILD_DEPENDS}.txt")
-    add_custom_command(
-        OUTPUT ${RUN_OUTPUT}
-        COMMAND ${RUN_AFTER_BUILD}
-        DEPENDS ${RUN_AFTER_BUILD_DEPENDS}
-        USES_TERMINAL
-    )
+# TODO to implement
+    message(WARNING "TO IMPLEMENT run after build: ${RUN_AFTER_BUILD}")
 
-    # Create target which consume the command via DEPENDS.
-    add_custom_target(run ALL DEPENDS ${RUN_OUTPUT})
+    # set(RUN_OUTPUT "${CMAKE_BINARY_DIR}/running_${RUN_AFTER_BUILD_DEPENDS}.txt")
+    # add_custom_command(
+    #     OUTPUT ${RUN_OUTPUT}
+    #     COMMAND ${RUN_AFTER_BUILD}
+    #     DEPENDS ${RUN_AFTER_BUILD_DEPENDS}
+    #     USES_TERMINAL
+    # )
+
+    # # Create target which consume the command via DEPENDS.
+    # add_custom_target(run ALL DEPENDS ${RUN_OUTPUT})
+
+    # add_custom_target(run COMMAND ${RUN_AFTER_BUILD} USES_TERMINAL)
+    # add_custom_command(TARGET ${RUN_AFTER_BUILD_DEPENDS} POST_BUILD COMMAND ${RUN_AFTER_BUILD} USES_TERMINAL)
 endif()
