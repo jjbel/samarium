bfc04f23571fec726cf7abe3236c219d3a5e2ef2
jjbel <jaibellare@gmail.com>
2024-09-17 00:01:25 +0530

    examples: zoom: cleanup

--- examples/graphics/zoom.cpp
+++ examples/graphics/zoom.cpp
@@ -6,49 +6,26 @@ using namespace sm::literals;
 
 auto main() -> i32
 {
-    auto window = Window{{.dims{1200, 600}}};
+    auto window = Window{{.dims = dims720}};
 
     auto draw = [&]
     {
         draw::background("#07090b"_c);
 
         constexpr auto a = 0.2;
-        for (auto pos : std::vector<Vector2>{{0, 0}, {a, a}, {-a, a}, {a, -a} /* , {-a, -a} */})
+        for (auto pos : std::vector<Vector2>{{0, 0}, {a, a}, {-a, a}, {a, -a}})
         {
             draw::circle(window, Circle{pos, 0.08}, "#ff0000"_c, 64);
         }
     };
 
-    // TODO on the 2nd frame, mouse delta is non-zero
-    // can fix by calling display twice, or maybe getInput twice?
-    // window.display();
-    // window.display();
-    auto count = 0;
-    auto watch = Stopwatch{};
+    run(window,
+        [&]
+        {
+            window.pan();
+            window.zoom_to_cursor();
+            // window.zoom_to_origin();
 
-    while (window.is_open())
-    {
-        window.pan();
-        window.zoom_to_cursor();
-        // window.zoom_to_origin();
-
-        // TODO sleep or stopwatch not accurate? 4ms makes it go to 16ms
-        // std::this_thread::sleep_for(std::chrono::milliseconds(4));
-
-        count++;
-        draw();
-        window.display();
-    }
-
-    const auto fps = count / watch.seconds();
-    const auto ms  = 1000.0 / fps;
-    print(fmt::format("{: 6.2f}fps, {: 5.2f}ms", fps, ms));
+            draw();
+        });
 }
-
-// TODO dont pass glm mat4x4 to shader at all (can use the overloads in future)
-// include "glm/gtx/string_cast.hpp"     // TODO remove for to_string
-// TODO shd include <glm/gtc/type_ptr.hpp>
-
-// TODO header only makes it faster?
-// TODO resizing window: see glViewport. draw 4 circles at GL corners and see if they remain at
-// corners
