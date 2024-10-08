d1627b2bc34b7ae41cb651e37ef34e45a4ed7c44
jjbel <jaibellare@gmail.com>
2024-10-08 20:21:17 +0530

    dev changes

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 @REM cls
-@REM cmake --build --preset=win && .\build\test\Release\samarium_tests
-cmake --build --preset=win && .\build\examples\Release\flow_field_noise
+cmake --build --preset=win && .\build\test\Release\samarium_tests
+@REM cmake --build --preset=win && .\build\examples\Release\flow_field_noise
