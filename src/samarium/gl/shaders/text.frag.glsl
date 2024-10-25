e03228ba5922d2d3ba7c80c785ae86980fcc2142
jjbel <jaibellare@gmail.com>
2024-10-25 23:39:43 +0530

    shaders: text.frag: add comments

--- src/samarium/gl/shaders/text.frag.glsl
+++ src/samarium/gl/shaders/text.frag.glsl
@@ -8,7 +8,11 @@ out vec4 frag_color;
 
 void main()
 {
-    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(input_texture, tex_coord).r);
+    // the texture is only red channel, use it to make an alpha map
+    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(input_texture, tex_coord));
     frag_color        = color * sampled;
+
+    // use this to view just the bounding boxes of the chars
+    // frag_color     = color;
 }
 )glsl"
