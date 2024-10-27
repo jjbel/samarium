8723314a05dd0cb354d6b584ded8a7ea819461ee
jjbel <jaibellare@gmail.com>
2024-10-27 13:55:12 +0530

    test_package: fix conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,6 +3,7 @@ import os
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.build import cross_building
+from conan.tools.build import can_run
 
 
 class TestPackageConan(ConanFile):
@@ -11,7 +12,37 @@ class TestPackageConan(ConanFile):
     test_type = "explicit"
 
     def requirements(self):
+        # TODO apparently have to propagate all deps
+        # and in configure() below, their options too...
+
+        deps = [
+            "fmt/11.0.2",
+            "range-v3/0.12.0",
+            "stb/cci.20240531",
+            "tl-expected/20190710",
+            "tl-function-ref/1.0.0",
+            "bshoshany-thread-pool/4.1.0",
+            "unordered_dense/4.4.0",
+            "svector/1.0.3",
+            "glfw/3.4",
+            "glm/cci.20230113",
+            "glad/0.1.36",
+            "freetype/2.13.2",
+        ]
+
+        for dep in deps:
+            self.requires(dep)
+
         self.requires(self.tested_reference_str)
+
+    def configure(self):
+        self.options["glad"].gl_profile = "core"
+        self.options["glad"].gl_version = "4.6"
+
+        self.options["freetype"].with_png = False
+        self.options["freetype"].with_zlib = False
+        self.options["freetype"].with_bzip2 = False
+        self.options["freetype"].with_brotli = False
 
     def layout(self):
         cmake_layout(self)
@@ -21,7 +52,10 @@ class TestPackageConan(ConanFile):
         cmake.configure()
         cmake.build()
 
+    # https://docs.conan.io/2/tutorial/creating_packages/test_conan_packages.html#tutorial-creating-test
     def test(self):
-        if not cross_building(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            # # the executable name ie, the one in CMake add_executable
+            # here it's test_package
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
